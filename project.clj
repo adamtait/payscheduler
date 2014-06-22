90ba820d28accbd9a421d795afc5507756ca9b23
Adam Tait <adam@adamtait.com>
2014-06-21 18:10:55 -0700

    add main namespace

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject payscheduler "0.1.0-SNAPSHOT"
   :description "a simple app for making a Venmo payment. can be used to schedule payments, in combination with cron."
-  :url "http://adamtait.com/payscheduler"
+  :url "http://payscheduler.adamtait.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.5"]
-                 [clj-http "0.9.2"]])
+                 [clj-http "0.9.2"]]
+  :main ^:skip-aot com.adamtait.payscheduler)
